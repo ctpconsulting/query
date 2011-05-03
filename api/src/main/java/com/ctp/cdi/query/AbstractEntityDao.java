dfef6c6b7595e07719154ca95fbb46a569a3bd9f
thomashug <thomas.hug@gmail.com>
2011-05-03 17:19:26 +0200

    Decoupling criteria from EntityDao

--- api/src/main/java/com/ctp/cdi/query/AbstractEntityDao.java
+++ api/src/main/java/com/ctp/cdi/query/AbstractEntityDao.java
@@ -31,113 +31,98 @@ public abstract class AbstractEntityDao<E, PK extends Serializable> implements E
     
     protected abstract Class<E> entityClass();
     
-    protected Criteria criteria() {
-        return new Criteria();
+    protected Criteria<E> criteria() {
+        return new Criteria<E>(entityClass());
     }
     
-    protected class Criteria {
+    protected <T> Criteria<T> criteria(Class<T> clazz) {
+        return new Criteria<T>(clazz);
+    }
+    
+    protected class Criteria<C> {
         
         private CriteriaBuilder builder;
-        private CriteriaQuery<E> query;
-        private Root<E> root;
+        private CriteriaQuery<C> query;
+        private Root<C> root;
         
-        private boolean isAnd = true;
+        List<Predicate> predicate = new LinkedList<Predicate>();
         
-        List<Predicate> and = new LinkedList<Predicate>();
-        List<Predicate> or = new LinkedList<Predicate>();
-        
-        private Criteria() {
-            Class<E> entityClass = entityClass();
+        private Criteria(Class<C> entityClass) {
             builder = getEntityManager().getCriteriaBuilder();
             query = builder.createQuery(entityClass);
             root = query.from(entityClass);
         }
         
-        public TypedQuery<E> createQuery() {
-            query.where(
-                    builder.and(and.toArray(new Predicate[] {})), 
-                    builder.or(or.toArray(new Predicate[] {})));
+        public TypedQuery<C> createQuery() {
+            query.where(builder.and(predicate.toArray(new Predicate[] {})));
             return getEntityManager().createQuery(query);
         } 
         
         private void add(Predicate pred) {
-            if (isAnd)
-                and.add(pred);
-            else
-                or.add(pred);
+            predicate.add(pred);
         }
         
-        public Criteria and() {
-            isAnd = true;
-            return this;
-        }
-        
-        public Criteria or() {
-            isAnd = false;
-            return this;
-        }
-        
-        public <P> Criteria eq(SingularAttribute<E, P> att, P value) {
+        public <P> Criteria<C> eq(SingularAttribute<C, P> att, P value) {
             add(builder.equal(root.get(att), value));
             return this;
         }
         
-        public <P> Criteria notEq(SingularAttribute<E, P> att, P value) {
+        public <P> Criteria<C> notEq(SingularAttribute<C, P> att, P value) {
             add(builder.notEqual(root.get(att), value));
             return this;
         }
         
-        public <P> Criteria like(SingularAttribute<E, String> att, String value) {
+        public <P> Criteria<C> like(SingularAttribute<C, String> att, String value) {
             add(builder.like(root.get(att), value));
             return this;
         }
         
-        public <P> Criteria notLike(SingularAttribute<E, String> att, String value) {
+        public <P> Criteria<C> notLike(SingularAttribute<C, String> att, String value) {
             add(builder.notLike(root.get(att), value));
             return this;
         }
         
-        public <P extends Number> Criteria lt(SingularAttribute<E, P> att, P value) {
+        public <P extends Number> Criteria<C> lt(SingularAttribute<C, P> att, P value) {
             add(builder.lt(root.get(att), value));
             return this;
         }
         
-        public <P extends Comparable<? super P>> Criteria ltOrEq(SingularAttribute<E, P> att, P value) {
+        public <P extends Comparable<? super P>> Criteria<C> ltOrEq(SingularAttribute<C, P> att, P value) {
             add(builder.lessThanOrEqualTo(root.get(att), value));
             return this;
         }
         
-        public <P extends Number> Criteria gt(SingularAttribute<E, P> att, P value) {
+        public <P extends Number> Criteria<C> gt(SingularAttribute<C, P> att, P value) {
             add(builder.gt(root.get(att), value));
             return this;
         }
         
-        public <P extends Comparable<? super P>> Criteria gtOrEq(SingularAttribute<E, P> att, P value) {
+        public <P extends Comparable<? super P>> Criteria<C> gtOrEq(SingularAttribute<C, P> att, P value) {
             add(builder.greaterThanOrEqualTo(root.get(att), value));
             return this;
         }
         
-        public <P extends Comparable<? super P>> Criteria between(SingularAttribute<E, P> att, P lower, P upper) {
+        public <P extends Comparable<? super P>> Criteria<C> between(SingularAttribute<C, P> att, P lower, P upper) {
             add(builder.between(root.get(att), lower, upper));
             return this;
         }
         
-        public <P> Criteria isNull(SingularAttribute<E, P> att) {
+        public <P> Criteria<C> isNull(SingularAttribute<C, P> att) {
             add(builder.isNull(root.get(att)));
             return this;
         }
         
-        public <P> Criteria notNull(SingularAttribute<E, P> att) {
+        public <P> Criteria<C> notNull(SingularAttribute<C, P> att) {
             add(builder.isNotNull(root.get(att)));
             return this;
         }
         
-        public <P extends Collection<?>> Criteria empty(SingularAttribute<E, P> att) {
+        public <P extends Collection<?>> Criteria<C> empty(SingularAttribute<C, P> att) {
             add(builder.isEmpty(root.get(att)));
             return this;
         }
         
-        public <P extends Collection<?>> Criteria notEmpty(SingularAttribute<E, P> att) {
+        public <P extends Collection<?>> Criteria<C> notEmpty(SingularAttribute<C, P> att) {
             add(builder.isNotEmpty(root.get(att)));
             return this;
         }
