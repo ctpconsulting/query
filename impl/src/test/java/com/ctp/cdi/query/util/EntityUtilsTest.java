285dce345e394595c35467aba3b489ad0ac8daa2
Romain Manni-Bucau <rmannibucau@gmail.com>
2012-11-15 09:25:06 +0100

    using JPA 2 feature to test if an entity is new or not

--- impl/src/test/java/com/ctp/cdi/query/util/EntityUtilsTest.java
+++ impl/src/test/java/com/ctp/cdi/query/util/EntityUtilsTest.java
@@ -14,31 +14,6 @@ import com.ctp.cdi.query.test.domain.Simple;
 public class EntityUtilsTest {
 
     @Test
-    public void shouldDetectNewEntity() {
-        // given
-        Simple simple = new Simple();
-
-        // when
-        boolean isNew = EntityUtils.isNew(simple);
-
-        // then
-        Assert.assertTrue(isNew);
-    }
-
-    @Test
-    public void shouldDetectPersistedEntity() {
-        // given
-        Simple simple = new Simple();
-        simple.setId(Long.valueOf(0));
-
-        // when
-        boolean isNew = EntityUtils.isNew(simple);
-
-        // then
-        Assert.assertFalse(isNew);
-    }
-
-    @Test
     public void shouldFindIdPropertyClass() {
         // given
 
