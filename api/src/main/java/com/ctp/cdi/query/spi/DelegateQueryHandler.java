6d56513e54af23d25f6e46bfaea5158f5464ea71
Thomas Hug <Thomas.Hug@ctp-consulting.com>
2013-01-17 14:27:11 -0800

    Preparing Alpha5: Bring documentation up-to-date. Still relies on the old Seam parent POM for docbook. Should migrate this to awestruct...

--- api/src/main/java/com/ctp/cdi/query/spi/DelegateQueryHandler.java
+++ api/src/main/java/com/ctp/cdi/query/spi/DelegateQueryHandler.java
@@ -4,27 +4,27 @@ package com.ctp.cdi.query.spi;
 
 /**
  * A marker interface. Used for writing custom query methods:
- * 
+ *
  * public interface DaoExtension<E> {
  *     E saveAndFlushAndRefresh(E entity);
  * }
- * 
+ *
  * public class DelegateDaoExtension<E> implements DaoExtension<E>, DelegateQueryHandler {
  *    @Inject
  *    private QueryInvocationContext context;
  *
  *    @Override
  *    public E saveAndFlushAndRefresh(E entity) {
- *        ... 
+ *        ...
  *    }
  * }
- * 
+ *
  * The extension is now usable with:
- * 
+ *
  * @Dao
- * public interface MySimpleDao extends MyEntityDao<Simple>, EntityDao<Simple, Long> {
+ * public interface MySimpleDao extends DaoExtension<Simple>, EntityDao<Simple, Long> {
  * }
- * 
+ *
  */
 public interface DelegateQueryHandler {
 }
